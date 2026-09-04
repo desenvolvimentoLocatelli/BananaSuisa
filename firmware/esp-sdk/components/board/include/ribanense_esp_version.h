72d3beaf4dd8b77af22371a826135294463c52ff
desenvolvimentoLocatelli <desenvolvimento@locatelli.com.br>
2026-09-04 03:54:55 -0300

    chore(release): bump de versoes (publish all)
     - Launcher 0.1.16 -> 0.1.17
     - OS 0.3.3 -> 0.3.4
     - Sobre 0.1.1 -> 0.1.2

--- firmware/esp-sdk/components/board/include/ribanense_esp_version.h
+++ firmware/esp-sdk/components/board/include/ribanense_esp_version.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define RIBANENSEESP_PRODUCT "RibanenseESP"
-#define RIBANENSEESP_VERSION "0.3.3"
+#define RIBANENSEESP_VERSION "0.3.4"
 #define RIBANENSEESP_OTA_KEY "ribanense-esp"
 #define RIBANENSEESP_MANIFEST_URL \
     "https://raw.githubusercontent.com/desenvolvimentoLocatelli/BananaSuisa/main/firmware/ribanense-esp/firmware.json"
@@ -9,6 +9,7 @@
     "https://raw.githubusercontent.com/desenvolvimentoLocatelli/BananaSuisa/main/catalog/esp-catalog.json"
 #define RIBANENSEESP_GITHUB_OWNER "desenvolvimentoLocatelli"
 #define RIBANENSEESP_GITHUB_REPO  "BananaSuisa"
+
 
 
 
